606cc7ab70635b6c179ecaacf3aad54137855ab6
Kateryna Semeniuk <katyasemeniuk@gmail.com>
2024-11-12 14:05:21 +0000

    created the TaskDetail view to retrieve, edit and delete it

--- tasks/views.py
+++ tasks/views.py
@@ -28,3 +28,36 @@ class TaskList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
         
+
+class TaskDetail(APIView):
+    """
+    Retrieve, update or delete a task if you're the owner.
+    """
+    serializer_class = TaskSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            task = Task.objects.get(pk=pk)
+            self.check_object_permissions(self.request, task)
+            return task
+        except Task.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(task, context={'request': request})
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(task, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        task = self.get_object(pk)
+        task.delete()
+        return Response( status = status.HTTP_204_NO_CONTENT)
