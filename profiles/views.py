9904915c4edb79332ec60838e75f6d896a2d2d58
Kateryna Semeniuk <katyasemeniuk@gmail.com>
2025-01-11 10:37:56 +0000

    adds deletion of the task

--- profiles/views.py
+++ profiles/views.py
@@ -5,7 +5,7 @@ from .serializers import ProfileSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
-
+from rest_framework.exceptions import PermissionDenied
 
 
 class ProfileList(generics.ListAPIView):
@@ -16,29 +16,34 @@ class ProfileList(generics.ListAPIView):
 
     serializer_class = ProfileSerializer
     queryset = Profile.objects.annotate(
-        tasks_count = Count('owner__task', distinct=True),
+        tasks_count=Count('owner__task', distinct=True),
     ).order_by('-created_at')
 
     filter_backends = [
         filters.OrderingFilter,
         DjangoFilterBackend,
-        SearchFilter, 
+        SearchFilter,
     ]
 
     ordering_fields = [
         'tasks_count',
     ]
 
-    search_fields = ['owner__username'] 
+    search_fields = ['owner__username']
 
 
-
-class ProfileDetail(generics.RetrieveUpdateAPIView):
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    Retrieve or update a profile if you're the owner.
+    Retrieve, update, or delete a profile if you're the owner.
     """
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.annotate(
-        tasks_count = Count('owner__tasks', distinct=True),
+        tasks_count=Count('owner__tasks', distinct=True),
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
+
+    def perform_destroy(self, instance):
+        # Check if the current user is the owner of the profile
+        if instance.owner != self.request.user:
+            raise PermissionDenied("You do not have permission to delete this profile.")
+        instance.delete()
